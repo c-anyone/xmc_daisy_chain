644c254795cae10e7b1c0208288498ca8c3e48e7
zwarbl <stophilos@gmail.com>
2016-10-21 00:21:35 +0200

    removed the dependance on dave.h, but uart still has to be setup before calling uartCobsInit, so it can still be done via dave

--- uart_cobs.c
+++ uart_cobs.c
@@ -2,22 +2,21 @@
  * uart_cobs.c
  *
  *  Created on: Oct 19, 2016
- *      Author: Faebsn
+ *      Author: Fabio Pungg
  */
 
-#include <DAVE.h>
+#include <xmc_usic.h>
+#include <xmc_uart.h>
 #include "uart_cobs.h"
 #include "cobs.h"
 
-static UART_CONFIG_t *uart_cobs_config = NULL;
+// static UART_CONFIG_t *uart_cobs_config = NULL;
+static XMC_USIC_CH_t *usic_channel = NULL;
 
 static uint8_t txBuf[MAX_FRAME_SIZE];
-static size_t tx_count = 0;
-static uint8_t *sendPosition;
-static uint8_t rx_buf[MAX_FRAME_SIZE];	//buffer to poll the bytes
+static size_t tx_count = 0, curTxPos = 0;
+static uint8_t rx_buf[MAX_FRAME_SIZE];	//buffer to poll the bytes into
 uint8_t buf[MAX_FRAME_SIZE]; // buffer to hand to upper layer
-
-static size_t txFifoSize = 0;
 
 typedef enum {
 	UART_TX_IDLE, UART_TX_WORKING, UART_TX_ENDING
@@ -28,30 +27,29 @@ static tx_state_t uart_tx_state = UART_TX_IDLE;
 /*
  * Hand over control of the uart to uart_cobs module
  */
-void uartCobsInit(UART_CONFIG_t *uartConfig) {
-	if (uartConfig == NULL)
+void uartCobsInit(XMC_USIC_CH_t *xmc_usic_ch) {
+	if (xmc_usic_ch == NULL)
 		return;
 
-	uart_cobs_config = uartConfig;
-	txFifoSize = 1U << uartConfig->config->tx_fifo_size;
-	XMC_UART_CH_Start(uart_cobs_config->channel);
+	usic_channel = xmc_usic_ch;
+	XMC_UART_CH_Start(usic_channel);
 }
 
 static void uartPutData(void) {
-	size_t i, length;
-	if (tx_count > txFifoSize) {
-		uart_tx_state = UART_TX_WORKING;
-		length = txFifoSize;
-		tx_count -= txFifoSize;
-	} else {
+	uint8_t tmp;
+	while ((tmp = (!XMC_USIC_CH_TXFIFO_IsFull(usic_channel)))
+			&& curTxPos <= tx_count) {
+		XMC_USIC_CH_TXFIFO_PutData(usic_channel,
+				(uint16_t) txBuf[curTxPos]);
+		++curTxPos;
+	}
+	if (curTxPos >= tx_count) {
 		uart_tx_state = UART_TX_ENDING;
-		length = tx_count;
+		curTxPos = 0;
 		tx_count = 0;
+	} else {
+		uart_tx_state = UART_TX_WORKING;
 	}
-	for (i = 0; i < length; ++i) {
-		XMC_USIC_CH_TXFIFO_PutData(uart_cobs_config->channel, (uint16_t) sendPosition[i]);
-	}
-	sendPosition += length;
 }
 
 void uart_cobs_transmit(uint8_t *data, size_t length) {
@@ -60,9 +58,6 @@ void uart_cobs_transmit(uint8_t *data, size_t length) {
 	}
 	// cobs encode the data before transmission
 	tx_count = cobs_encode(data, length, txBuf);
-	// set the sendPosition to start of the buffer
-	sendPosition = txBuf;
-
 	uartPutData();
 
 }
@@ -83,51 +78,16 @@ void uartCobsTransmitIRQ(void) {
 void pollUartCobs(void) {
 	uint8_t tmpval;
 	static size_t count = 0;
-	while (!XMC_USIC_CH_RXFIFO_IsEmpty(uart_cobs_config->channel)
+	while (!XMC_USIC_CH_RXFIFO_IsEmpty(usic_channel)
 			&& count < MAX_FRAME_SIZE) {
-		tmpval = (uint8_t) XMC_UART_CH_GetReceivedData(
-				uart_cobs_config->channel);
+		tmpval = (uint8_t) (XMC_USIC_CH_RXFIFO_GetData(
+				usic_channel) & 0xFF);
 		rx_buf[count++] = tmpval;
 		if (tmpval == '\0') {
 			count = cobs_decode(rx_buf, count, buf); // decode the original data
 			uartCobsFrameReceived(buf, count);		// pass to upper layer
 			count = 0;
+			return;
 		}
 	}
 }
-
-/*
- int uartCobsWorker() {
- uint8_t tmpval;
- static size_t count=0;
- if (rx_flag != 0) {
- rx_flag = 0;
- while((tmpval = rb_pop(&rx_fifo)) != 0) {
- rx_buf[count] = tmpval;
- ++count;
- }
- return 0;
- }
- return 1;
- }
- */
-size_t uart_cobs_frame_received(uint8_t *buf, size_t length);
-
-/*
- * when the uart fifo is filled, read data from fifo and put it into rbuf
- */
-/*
- void uartReceiveIRQ() {
- uint16_t rxData = 0;
- while (!XMC_USIC_CH_RXFIFO_IsEmpty(uart_cobs_config->channel)
- && count < UART_MAX_FRAME_SIZE) {
- rxData = XMC_UART_CH_GetReceivedData(uart_cobs_config->channel);
- rb_push(rx_fifo, rxData & 0xFF);
- if (rxData == 0) {
- rx_flag = 1;
- return;
- }
- }
- }
-
- */
--- uart_cobs.h
+++ uart_cobs.h
@@ -5,18 +5,16 @@
  *      Author: Faebsn
  */
 
-#ifndef XMC_DAISYCHAIN_UART_COBS_H_
-#define XMC_DAISYCHAIN_UART_COBS_H_
+#ifndef UART_COBS_H_
+#define UART_COBS_H_
 
 #define MAX_FRAME_SIZE 64 // including frame-delimiter
 
-void uartCobsInit(UART_CONFIG_t *uartConfig);
+void uartCobsInit(XMC_USIC_CH_t *uartConfig);
 void pollUartCobs(void);
 void uart_cobs_transmit(uint8_t *data, size_t length);
 
 
 extern void uartCobsFrameReceived(uint8_t *frame,size_t length);
 
-//int uartCobsWorker()
-
-#endif /* XMC_DAISYCHAIN_UART_COBS_H_ */
+#endif /* UART_COBS_H_ */
