3e5f21ed4942fbd4a85c6ae92e67b923d1b7f97d
Zenbook\faebsn <stophilos@gmail.com>
2016-10-21 11:43:07 +0200

    reworking daisyChain to use uart_cobs as lower layer, receive callback and transmit function including crc  implemented for now

--- DaisyChain.h
+++ DaisyChain.h
@@ -8,8 +8,6 @@
 #ifndef DAISYCHAIN_H_
 #define DAISYCHAIN_H_
 
-#include "min.h"
-
 /*
  * special addresses for different purposes
  */
@@ -17,12 +15,20 @@
 #define DAISY_ADDR_COUNT		(0x7fu)
 #define DAISY_ERROR				(0x8fu)
 
+ typedef enum {
+ DAISY_NONE,
+ DAISY_AUTO_DISCOVER,
+ DAISY_PING,
+ DAISY_SET_ALL,
+ DAISY_RESET_ALL
+ } DAISY_CHAIN_COMMANDS_t;
+
 // typedef for data receive callback function
  typedef void (*daisyRxCallback_t)(uint8_t,uint8_t,uint8_t*);
 
 void daisyInit(UART_CONFIG_t *uartConfig);
 
-void daisySendData(uint8_t address,uint8_t length,uint8_t* data);
+void daisySendData(uint8_t receiver,uint8_t sender,uint8_t* data, size_t length);
 
 void daisySetRxCallback(daisyRxCallback_t);
 
--- uart_cobs.h
+++ uart_cobs.h
@@ -8,11 +8,11 @@
 #ifndef UART_COBS_H_
 #define UART_COBS_H_
 
-#define MAX_FRAME_SIZE 64 // including frame-delimiter
+#define COBS_MAX_FRAME_SIZE 64 // including frame-delimiter
 
 void uartCobsInit(XMC_USIC_CH_t *uartConfig);
-void pollUartCobs(void);
-void uart_cobs_transmit(uint8_t *data, size_t length);
+void uartCobsPoll(void);
+void uartCobsTransmit(uint8_t *data, size_t length);
 
 
 extern void uartCobsFrameReceived(uint8_t *frame,size_t length);
